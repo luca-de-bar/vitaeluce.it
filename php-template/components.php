3e8208048afe06b4ff9e000dcc5df8830abb1e23
Koteyo <lucadebar33@gmail.com>
2023-11-18 22:39:15 +0100

    components.php

--- php-template/components.php
+++ php-template/components.php
@@ -1,33 +1,16 @@
 <?php
-function head() {
-    $output = '<head>';
-    $output .= '<meta charset="utf-8">';
-    $output .= '<meta http-equiv="x-ua-compatible" content="ie=edge">';
-    $output .= '<title>Vita e Luce - Missione Zigana Evangelica</title>';
-    $output .= '<meta name="robots" content="index, follow" />';
-    $output .= '<meta name="description" content="Vita e Luce è una missione evangelica dedicata a stimolare la fede attraverso la musica, la preghiera e l\'insegnamento della Parola di Dio. Unisciti a noi per arricchire la tua vita spirituale.">';
-    $output .= '<meta name="keywords" content="Vita e Luce, missione evangelica, fede cristiana, musica religiosa, preghiera, scuola domenicale, Dio">';
-    $output .= '<link rel="canonical" href="https://www.vitaeluce.it/" />';
-    $output .= '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">';
-    $output .= '<link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.webp">';
-    $output .= '<link rel="stylesheet" href="assets/css/vendor/bootstrap.min.css">';
-    $output .= '<link rel="stylesheet" href="assets/css/vendor/font-awesome-pro.min.css">';
-    $output .= '<link rel="stylesheet" href="assets/css/vendor/themify-icons.css">';
-    $output .= '<link rel="stylesheet" href="assets/css/vendor/customFonts.css">';
-    $output .= '<link rel="stylesheet" href="assets/css/plugins/select2.min.css">';
-    $output .= '<link rel="stylesheet" href="assets/css/plugins/perfect-scrollbar.css">';
-    $output .= '<link rel="stylesheet" href="assets/css/plugins/swiper.min.css">';
-    $output .= '<link rel="stylesheet" href="assets/css/plugins/nice-select.css">';
-    $output .= '<link rel="stylesheet" href="assets/css/plugins/ion.rangeSlider.min.css">';
-    $output .= '<link rel="stylesheet" href="assets/css/plugins/photoswipe.css">';
-    $output .= '<link rel="stylesheet" href="assets/css/plugins/photoswipe-default-skin.css">';
-    $output .= '<link rel="stylesheet" href="assets/css/plugins/magnific-popup.css">';
-    $output .= '<link rel="stylesheet" href="assets/css/plugins/slick.css">';
-    $output .= '<link rel="stylesheet" href="assets/css/custom.css">';
-    $output .= '<link rel="stylesheet" href="assets/css/style.min.css">';
-    $output .= '</head>';
+function meta_and_title() {
+    return "
+    <title>Vita e Luce - Missione Zigana Evangelica</title>
+    <meta name=\"robots\" content=\"index, follow\" />
+    <meta name=\"description\" content=\"Vita e Luce è una missione evangelica dedicata a stimolare la fede attraverso la musica, la preghiera e l'insegnamento della Parola di Dio. Unisciti a noi per arricchire la tua vita spirituale.\">
+    <meta name=\"keywords\" content=\"Vita e Luce, missione evangelica, fede cristiana, musica religiosa, preghiera, scuola domenicale, Dio\">
+    ";
+}
 
-    return $output;
+function favicon() {
+    return "    <link rel=\"shortcut icon\" type=\"image/x-icon\" href=\"assets/images/favicon.webp\">
+    ";
 }
 
 ?>
--- php-template/index.php
+++ php-template/index.php
@@ -1,26 +1,13 @@
 <?php include 'components.php';?>
 <!DOCTYPE html>
-<html class="no-js" lang="en">
-    <?php echo head(); ?>
-    <body>
-        <?php echo preloader(); ?>
-        <?php echo headerTop(); ?>
-        <?php echo headerMiddle(); ?>
-        <?php echo headerBottom(); ?>
-        <?php echo mobileMenu(); ?>
-        <?php echo slider(); ?>
-        <?php echo about(); ?>
-        <?php echo services(); ?>
-        <?php echo team(); ?>
-        <?php echo testimonial(); ?>
-        <?php echo blog(); ?>
-        <?php echo contact(); ?>
-        <?php echo footer(); ?>
-        <?php echo backToTop(); ?>
-        <?php echo modalSearch(); ?>
-        <?php echo modalShoppingCart(); ?>
-        <?php echo modalQuickView(); ?>
-        <?php echo modalWishlist(); ?>
-        <?php echo modalCompare(); ?>
-        <?php echo js(); ?>
-    </body>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php echo meta_and_title(); ?>
+    <?php echo favicon(); ?>
+</head>
+<body>
+    
+</body>
+</html>
