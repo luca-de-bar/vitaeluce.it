b880aa9f7c19af38ea3516750b9e55a9baf7d053
Koteyo <lucadebar33@gmail.com>
2023-11-18 22:09:14 +0100

    try-components

---
+++ php-template/index.php
@@ -0,0 +1,26 @@
+<?php include 'components.php';?>
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+    <?php echo head(); ?>
+    <body>
+        <?php echo preloader(); ?>
+        <?php echo headerTop(); ?>
+        <?php echo headerMiddle(); ?>
+        <?php echo headerBottom(); ?>
+        <?php echo mobileMenu(); ?>
+        <?php echo slider(); ?>
+        <?php echo about(); ?>
+        <?php echo services(); ?>
+        <?php echo team(); ?>
+        <?php echo testimonial(); ?>
+        <?php echo blog(); ?>
+        <?php echo contact(); ?>
+        <?php echo footer(); ?>
+        <?php echo backToTop(); ?>
+        <?php echo modalSearch(); ?>
+        <?php echo modalShoppingCart(); ?>
+        <?php echo modalQuickView(); ?>
+        <?php echo modalWishlist(); ?>
+        <?php echo modalCompare(); ?>
+        <?php echo js(); ?>
+    </body>
