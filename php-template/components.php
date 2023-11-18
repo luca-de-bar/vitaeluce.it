42eb1896abfb361194c7e14559343a3b7dc24a29
Koteyo <lucadebar33@gmail.com>
2023-11-18 23:06:00 +0100

    css-aligned-with-main-branch

--- php-template/components.php
+++ php-template/components.php
@@ -5,12 +5,58 @@ function meta_and_title() {
     <meta name=\"robots\" content=\"index, follow\" />
     <meta name=\"description\" content=\"Vita e Luce è una missione evangelica dedicata a stimolare la fede attraverso la musica, la preghiera e l'insegnamento della Parola di Dio. Unisciti a noi per arricchire la tua vita spirituale.\">
     <meta name=\"keywords\" content=\"Vita e Luce, missione evangelica, fede cristiana, musica religiosa, preghiera, scuola domenicale, Dio\">
+    <link rel=\"canonical\" href=\"https://www.vitaeluce.it/\" />
     ";
 }
 
 function favicon() {
-    return "    <link rel=\"shortcut icon\" type=\"image/x-icon\" href=\"assets/images/favicon.webp\">
+    return "    <link rel=\"shortcut icon\" type=\"image/x-icon\" href=\"/assets/images/favicon.webp\">
     ";
 }
+
+
+function DesktopMenu() {
+    return <<<HTML
+    <!-- Header Section Start -->
+    <div class="header-section section section-fluid bg-white d-none d-xl-block">
+        <div class="container">
+            <div class="row align-items-center">
+
+                <!-- Header Logo Start -->
+                <div class="col-auto">
+                    <div class="header-logo">
+                        <a href="index.html"><img src="assets/images/logo/logo_transparent_background.png" alt="" style="max-width:200px; max-height:150px;"></a>
+                    </div>
+                </div>
+                <!-- Header Logo End -->
+
+                <!-- Search Start -->
+                <div class="col-auto me-auto">
+                    <nav class="site-main-menu site-main-menu-left menu-height-100 justify-content-center">
+                        <ul>
+                            <li class="Home"><a href="index.html"><span class="menu-text">Home</span></a></li>
+                            <li class="Chi Siamo"><a href="scopridipiu.html"><span class="menu-text">Chi Siamo</span></a></li>
+                            <li class="shop"><a href="spoiler.html"><span class="menu-text">CD Nuovi</span></a></li>
+                            <li class="uniti"><a href="unitiancheselontani.html"><span class="menu-text">#unitiancheselontani</span></a></li>
+                            <li class="preghiera"><a href="contattaci.html"><span class="menu-text">Richieste</span></a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <!-- Search End -->
+
+                <!-- Header Tools Start -->
+                <div class="col-auto">
+                    <div class="header-tools justify-content-end">
+                    </div>
+                </div>
+                <!-- Header Tools End -->
+
+            </div>
+        </div>
+    </div>
+    <!-- Header Section End -->
+HTML;
+}
+
 
 ?>
--- php-template/index.php
+++ php-template/index.php
@@ -2,12 +2,38 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Vita e Luce - Missione Zigana Evangelica</title>
     <?php echo meta_and_title(); ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicon -->
     <?php echo favicon(); ?>
+    <!-- CSS
+
+    <!-- Vendor CSS (Bootstrap & Icon Font) -->
+    <link rel="stylesheet" href="/assets/css/vendor/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/vendor/font-awesome-pro.min.css">
+    <link rel="stylesheet" href="/assets/css/vendor/themify-icons.css">
+    <link rel="stylesheet" href="/assets/css/vendor/customFonts.css">
+
+    <!-- Plugins CSS (All Plugins Files) -->
+    <link rel="stylesheet" href="/assets/css/plugins/select2.min.css">
+    <link rel="stylesheet" href="/assets/css/plugins/perfect-scrollbar.css">
+    <link rel="stylesheet" href="/assets/css/plugins/swiper.min.css">
+    <link rel="stylesheet" href="/assets/css/plugins/nice-select.css">
+    <link rel="stylesheet" href="/assets/css/plugins/ion.rangeSlider.min.css">
+    <link rel="stylesheet" href="/assets/css/plugins/slick.css">
+    <link rel="stylesheet" href="/assets/css/plugins/photoswipe.css">
+    <link rel="stylesheet" href="/assets/css/style.min.css">
+    <link rel="stylesheet" href="/assets/css/plugins/photoswipe-default-skin.css">
+    <link rel="stylesheet" href="/assets/css/custom.css">
+    <link rel="stylesheet" href="/assets/css/plugins/magnific-popup.css">
+
 </head>
+
 <body>
+    <?php echo DesktopMenu(); ?>
     
 </body>
 </html>
