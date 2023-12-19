a9070d60dfa6d2657e155227698541512f23c1cd
Koteyo <lucadebar33@gmail.com>
2023-12-19 13:13:57 +0100

    contattaci to richieste

--- components/menu.php
+++ components/menu.php
@@ -20,7 +20,7 @@
                             <li class="shop"><a href="spoiler.php"><span class="menu-text">CD Nuovi</span></a></li>
                             <li class="scuola"><a href="scuola-domenicale.php"><span class="menu-text">Scuola Domenicale</span></a></li>
                             <li class="uniti"><a href="unitiancheselontani.php"><span class="menu-text">#unitiancheselontani</span></a></li>
-                            <li class="preghiera"><a href="contattaci.php"><span class="menu-text">Richieste</span></a></li>
+                            <li class="preghiera"><a href="richieste.php"><span class="menu-text">Richieste</span></a></li>
                         </ul>
                     </nav>
                 </div>
@@ -153,7 +153,7 @@
                             <li class="shop"><a href="spoiler.php"><span class="menu-text">CD Nuovi</span></a></li>
                             <li class="scuola"><a href="scuola-domenicale.php"><span class="menu-text">Scuola Domenicale</span></a></li>
                             <li class="uniti"><a href="unitiancheselontani.php"><span class="menu-text">#unitiancheselontani</span></a></li>
-                            <li class="preghiera"><a href="contattaci.php"><span class="menu-text">Richieste</span></a></li>
+                            <li class="preghiera"><a href="richieste.php"><span class="menu-text">Richieste</span></a></li>
                         </ul>
             </div>
             <div class="offcanvas-buttons">
