a079cdb9228e53854aec6c72b3e5c72608e2ee19
Koteyo <lucadebar33@gmail.com>
2023-12-19 13:30:14 +0100

    testimonianze

---
+++ components/testimonianze.php
@@ -0,0 +1,55 @@
+    <!-- Testimonial Section Start -->
+    <div class="section section-padding">
+         <div class="container">
+            <div class="section-title2 row justify-content-between align-items-center">
+                <div class="col-md-auto col-12">
+                    <!-- Section Title Start -->
+                    <h2 class="title title-icon-right">Le nostre testimonianze</h2>
+                    <!-- Section Title End -->
+                </div>
+                <div class="col-md-auto col-12 mt-4 mt-md-0"></div>
+                </div>
+                <div class="testimonial-carousel">
+                    <div class="col">
+                        <div class="testimonial">
+                            <p>"Mentre pregavo avvertii un peso schiacciante dentro me: il peccato. E’ scritto infatti che “tutti hanno peccato e sono privi della gloria di Dio” (Romani 3:23) e per la prima volta compresi anche lo scopo per cui Gesù aveva così tanto patito quelle sofferenze sulla croce: affinché io e te fossimo liberi dal peccato per mezzo del Suo sacrificio perfetto."</p>
+                            <div class="author">
+                                <div class="content">
+                                    <h6 class="name">Simone</h6>
+                                </div>
+                         </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="testimonial">
+                            <p>"Iniziai a cercare Dio attraverso la lettura della Bibbia e la preghiera. Instaurai un rapporto personale con Dio. Con il tempo più Dio si faceva conoscere, più io più capivo quanto ero peccatrice. Ho chiesto perdono a Dio dei miei peccati e Dio ha iniziato la sua opera di trasformazione."</p>
+                            <div class="author">
+                                <div class="content">
+                                    <h6 class="name">Stefania</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="testimonial">
+                            <p>"In questo ultimo periodo, dovendo affrontare una situazione particolarmente difficile e delicata… ero distrutta sia dal punto di vista emotivo che spirituale. Ma il Signore, come sempre, era al mio fianco:Egli conosceva e vedeva ogni mia sofferenza e ha provveduto per me un’ aiuto speciale, particolare, inaspettato."</p>
+                            <div class="author">
+                                <div class="content">
+                                    <h6 class="name">Damaris</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="testimonial">
+                            <p>"A chiunque stia leggendo questa testimonianza, sappi che Dio ti ama, e che sei prezioso ai suoi occhi, non accettare il peccato e la sconfitta nella tua vita, perché il Signor Gesù ha riportato per te sulla croce una vittoria totale sul peccato e sulla morte, egli vive e vuol prendersi cura di te, della tua vita e della tua famiglia, proprio in questo momento invocalo ed egli verrà nel tuo cuore portando con se il suo regno."</p>
+                            <div class="author">
+                                <div class="content">
+                                    <h6 class="name">Antonino</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <!-- Testimonial Section End -->
