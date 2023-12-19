f3f1a1109112de071721e26795454953f004beed
Koteyo <lucadebar33@gmail.com>
2023-12-19 12:13:04 +0100

    footer php

---
+++ components/footer.php
@@ -0,0 +1,28 @@
+<footer>
+    <div class="footer2-section section section-padding">
+        <div class="container">
+            <div class="row learts-mb-n40">
+
+                <div class="col-lg-6 learts-mb-40">
+                    <div class="widget-about">
+                        <img src="assets/images/logo/logo_transparent_background.png" alt="" style="max-width:100%; max-height:150px;">
+                    </div>
+                </div>
+                <div class="col-lg-2 learts-mb-40">
+                    <ul class="widget-list">
+			            <li> <i class="fab fa-facebook-f"></i> <a href="https://www.facebook.com/adimezoff/" target="_blank">Facebook</a></li>
+                        <li> <i class="fab fa-instagram"></i> <a href="https://www.instagram.com/adi_mez_off/" target="_blank">Instagram</a></li>
+                        <li> <i class="fab fa-youtube"></i> <a href="https://www.youtube.com/@adi_mez_off" target="_blank">Youtube</a></li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="footer2-copyright section">
+        <div class="container">
+            <p class="copyright text-center">&copy; 2023 | Made with <i class="far fa-heart"></i> in Verona</p>
+        </div>
+    </div>
+</footer>
