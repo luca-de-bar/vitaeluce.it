145da76963bd959f240ff8a3ead0b0d1d9ee443d
Koteyo <lucadebar33@gmail.com>
2023-12-19 11:49:29 +0100

    tophp

---
+++ unitiancheselontani.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Vita e Luce - Missione Zigana Evangelica</title>
+    <meta name="robots" content="index, follow" />
+    <meta name="description" content="Vita e Luce è una missione evangelica dedicata a stimolare la fede attraverso la musica, la preghiera e l'insegnamento della Parola di Dio. Unisciti a noi per arricchire la tua vita spirituale.">
+    <meta name="keywords" content="Vita e Luce, missione evangelica, fede cristiana, musica religiosa, preghiera, scuola domenicale, Dio">
+    <link rel="canonical" href="https://www.vitaeluce.it/" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.webp">
+
+    <!-- CSS
+	============================================ -->
+
+    <!-- Vendor CSS (Bootstrap & Icon Font) -->
+    <link rel="stylesheet" href="assets/css/vendor/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/vendor/font-awesome-pro.min.css">
+    <link rel="stylesheet" href="assets/css/vendor/themify-icons.css">
+    <link rel="stylesheet" href="assets/css/vendor/customFonts.css">
+
+    <!-- Plugins CSS (All Plugins Files) -->
+    <link rel="stylesheet" href="assets/css/plugins/select2.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/perfect-scrollbar.css">
+    <link rel="stylesheet" href="assets/css/plugins/swiper.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/nice-select.css">
+    <link rel="stylesheet" href="assets/css/plugins/ion.rangeSlider.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/photoswipe.css">
+    <link rel="stylesheet" href="assets/css/plugins/photoswipe-default-skin.css">
+    <link rel="stylesheet" href="assets/css/plugins/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/plugins/slick.css">
+
+    <!-- Main Style CSS -->
+    <!--<link rel="stylesheet" href="assets/css/style.css"> -->
+    <link rel="stylesheet" href="assets/css/custom.css">
+
+    <!-- Use the minified version files listed below for better performance and remove the files listed above -->
+    <!-- <link rel="stylesheet" href="assets/css/vendor/vendor.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/plugins.min.css"> -->
+    <link rel="stylesheet" href="assets/css/style.min.css">
+
+</head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-6E07JQRRW4"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-6E07JQRRW4');
+</script>
+
+<body>
+ 
+
+<?php include 'menu.php';?>
+    <!-- Page Title/Header Start -->
+    <div class="page-title-section section" data-bg-image="assets/images/bg/page-title-1.webp">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+
+                    <div class="page-title">
+                        <h1 class="title">#unitiancheselontani</h1>
+                        <ul class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                            <li class="breadcrumb-item active">#unitiancheselontani</li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Page Title/Header End -->
+
+    <!-- Portfolio Section Start -->
+    <div class="section section-padding border-bottom">
+        <div class="container">
+            <div class="row learts-mb-n30">
+                <div class="col-xl-8 col-12 learts-mb-30">
+                    <div class="portfolio-image">
+                        <img src="assets/images/product/single/3/banner/uniti2.jpg" alt="">                     
+                    </div>
+                </div>
+
+                <div class="col-xl-4 col-12 learts-mb-30">
+                    <div class="portfolio-content">
+                        <h2 class="title">#unitiancheselontani</h2>
+                        <div class="desc">
+                            <p>Dalla missione <strong>"Vita e Luce"</strong> Nasce un gruppo di giovani cantanti e musicisti "Uniti anche se lontani".</p>
+                            <p>Nella scia dell'energia e dello spirito missionario della missione <strong>"Vita e Luce"</strong>, il gruppo di giovani cantanti e musicisti <strong>"Uniti anche se lontani"</strong> ha un obbiettivo : Espandere l'amore di Dio attraverso il potere della musica. Spinti dall'aspirazione di toccare le vite di quanti più ascoltatori possibile, hanno iniziato a lavorare su un album di debutto che incorpora temi di amore, perdono, e rinascita spirituale.</p>
+                        </div>
+                        <div class="portfolio-image">
+                            <img src="assets/images/portfolio/single/cdunitiancheselontani.jpeg" alt="">
+                        </div>
+                        <ul class="meta">
+                                <span class="name"><strong>Condividi:</strong></span>
+                                <span class="value social">
+                                    <a href="https://www.facebook.com/adimezoff/" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                                    <a href="https://www.instagram.com/adi_mez_off/" target="_blank"><i class="fab fa-instagram"></i></a>
+                                    <a href="https://www.youtube.com/@adi_mez_off" target="_blank"><i class="fab fa-youtube"></i></a>                    
+                                </span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+    <!-- Portfolio Section End -->
+
+    <!--Footer -->
+    <footer>
+        <div class="footer2-section section section-padding">
+            <div class="container">
+                <div class="row learts-mb-n40">
+    
+                    <div class="col-lg-6 learts-mb-40">
+                        <div class="widget-about">
+                            <img src="assets/images/logo/logo_transparent_background.png" alt="" style="max-width:100%; max-height:150px;">
+                        </div>
+                    </div>
+                    <div class="col-lg-2 learts-mb-40">
+                        <ul class="widget-list">
+                            <li> <i class="fab fa-facebook-f"></i> <a href="https://www.facebook.com/adimezoff/" target="_blank">Facebook</a></li>
+                            <li> <i class="fab fa-instagram"></i> <a href="https://www.instagram.com/adi_mez_off/" target="_blank">Instagram</a></li>
+                            <li> <i class="fab fa-youtube"></i> <a href="https://www.youtube.com/@adi_mez_off" target="_blank">Youtube</a></li>
+                        </ul>
+                    </div>
+    
+                </div>
+            </div>
+        </div>
+    
+        <div class="footer2-copyright section">
+            <div class="container">
+                <p class="copyright text-center">&copy; 2023 | Made with <i class="far fa-heart"></i> in Verona</p>
+            </div>
+        </div>
+    </footer>
+    
+
+    <!-- JS
+============================================ -->
+
+    <!-- Vendors JS -->
+    <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
+    <script src="assets/js/vendor/jquery-3.4.1.min.js"></script>
+    <script src="assets/js/vendor/jquery-migrate-3.1.0.min.js"></script>
+    <script src="assets/js/vendor/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugins JS -->
+    <script src="assets/js/plugins/select2.min.js"></script>
+    <script src="assets/js/plugins/jquery.nice-select.min.js"></script>
+    <script src="assets/js/plugins/perfect-scrollbar.min.js"></script>
+    <script src="assets/js/plugins/swiper.min.js"></script>
+    <script src="assets/js/plugins/slick.min.js"></script>
+    <script src="assets/js/plugins/mo.min.js"></script>
+    <script src="assets/js/plugins/jquery.ajaxchimp.min.js"></script>
+    <script src="assets/js/plugins/jquery.countdown.min.js"></script>
+    <script src="assets/js/plugins/imagesloaded.pkgd.min.js"></script>
+    <script src="assets/js/plugins/isotope.pkgd.min.js"></script>
+    <script src="assets/js/plugins/jquery.matchHeight-min.js"></script>
+    <script src="assets/js/plugins/ion.rangeSlider.min.js"></script>
+    <script src="assets/js/plugins/photoswipe.min.js"></script>
+    <script src="assets/js/plugins/photoswipe-ui-default.min.js"></script>
+    <script src="assets/js/plugins/jquery.zoom.min.js"></script>
+    <script src="assets/js/plugins/ResizeSensor.js"></script>
+    <script src="assets/js/plugins/jquery.sticky-sidebar.min.js"></script>
+    <script src="assets/js/plugins/product360.js"></script>
+    <script src="assets/js/plugins/jquery.magnific-popup.min.js"></script>
+    <script src="assets/js/plugins/jquery.scrollUp.min.js"></script>
+    <script src="assets/js/plugins/scrollax.min.js"></script>
+    <script src="assets/js/plugins/instafeed.min.js"></script>
+
+    <!-- Use the minified version files listed below for better performance and remove the files listed above -->
+    <!-- <script src="assets/js/vendor/vendor.min.js"></script>
+<script src="assets/js/plugins/plugins.min.js"></script> -->
+
+    <!-- Main Activation JS -->
+    <script src="assets/js/main.js"></script>
+    <!--
+    <script>
+        if (window.location.href.indexOf('.html') > -1) {
+  var updatedUrl = window.location.href.replace(/\.html/, '');
+  window.history.replaceState({}, '', updatedUrl);
+}
+
+    </script> 
+--> 
+</body>
+
+
+</html>
