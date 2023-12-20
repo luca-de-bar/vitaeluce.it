aec9baec834192462d1c2f38585c0d00e466fa37
Koteyo <lucadebar33@gmail.com>
2023-12-20 20:38:12 +0100

    footer absolute test 3

--- components/footer.php
+++ components/footer.php
@@ -1,11 +1,12 @@
 <footer>
+   <div class="relative"> 
     <div class="footer2-section section section-padding">
         <div class="container">
             <div class="row learts-mb-n40">
 
                 <div class="col-lg-6 learts-mb-40">
                     <div class="widget-about">
-                        <img src="assets/images/logo/logo_transparent_background.png" alt="" style="max-width:100%; max-height:150px;">
+                        <img src="assets/images/logo/logo_transparent_background.png" alt="" style="max-width:100%; max-height:150px;"/>
                     </div>
                 </div>
                 <div class="col-lg-2 learts-mb-40">
@@ -25,4 +26,5 @@
             <p class="copyright text-center">&copy; 2024 | Made with <i class="far fa-heart"></i> in Verona</p>
         </div>
     </div>
+  </div>
 </footer>
